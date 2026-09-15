cfa8948d2af27ca4c5e51243bb8e612cf64aeb89
Lukas Huber <lukas.huber@roamingstone.ch>
2026-09-15 20:33:53 +0200

    Added nix flake for development
    Signed-off-by: Lukas Huber <lukas.huber@roamingstone.ch>

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "GCC and other tooling.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.gcc
+            pkgs.gnumake
+            pkgs.gdb
+          ];
+        };
+      });
+}
